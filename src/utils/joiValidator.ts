c6960f4c41bc2b675f99a2a02c55b584dc1fdccd
Steven Bossio Leal <105065704+StxvenDev@users.noreply.github.com>
2024-08-30 18:01:36 -0500

    Update: joiValidator.ts

--- src/utils/joiValidator.ts
+++ src/utils/joiValidator.ts
@@ -1,6 +1,14 @@
 import Joi from "joi";
 
-export const bookSchema = Joi.object({
+export const bookSchemaCreate = Joi.object({
+  title : Joi.string().min(3).required(),
+  author : Joi.string().min(3).required(),
+  publicationDate : Joi.date().required(),
+  isbn : Joi.string().min(1).required(),
+  genre : Joi.string().min(1).required(),
+});
+
+export const bookSchemaUpdate = Joi.object({
   title : Joi.string().min(3),
   author : Joi.string().min(3),
   publicationDate : Joi.date(),
